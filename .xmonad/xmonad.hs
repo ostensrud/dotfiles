219655083e0500178979b02e0d5496ba4e8f3f85
Øivind Stensrud <oivind@oivind.net>
2017-04-08 17:47:04 +0200

    Switch workspace with numpad
    Easier with programmer dvorak

--- .xmonad/xmonad.hs
+++ .xmonad/xmonad.hs
@@ -128,6 +128,17 @@ myKeys conf@(XConfig {XMonad.modMask = modMask}) = M.fromList $
     [ ((modMask,                    xK_p        ), runOrRaisePrompt largeXPConfig),
       ((modMask,                    xK_Escape   ), spawn "setkeybl")
     ]
+    ++
+    [((m .|. modMask, k), windows $ f i)
+        | (i, k) <- zip myWorkspaces numPadKeys
+        , (f, m) <- [(W.greedyView, 0), (W.shift, shiftMask)]
+    ]
+
+-- Non-numeric num pad keys, sorted by number
+numPadKeys = [ xK_KP_End,  xK_KP_Down,  xK_KP_Page_Down -- 1, 2, 3
+             , xK_KP_Left, xK_KP_Begin, xK_KP_Right     -- 4, 5, 6
+             , xK_KP_Home, xK_KP_Up,    xK_KP_Page_Up   -- 7, 8, 9
+             , xK_KP_Insert]
 
 -- ManageHook {{{
 manageHook' :: ManageHook
